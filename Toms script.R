780e5aec3c02d50b7541a6472e7a79574e19a3e7
thomasmorgan <thomas.j.h.morgan@gmail.com>
2023-04-27 12:53:49 -0700

    Attempt at speeding things up
    Main thing is the hack,
    
    though data storage was slowing things down quite a bit too.

--- Toms script.R
+++ Toms script.R
@@ -1,3 +1,5 @@
+hack <- TRUE
+
 ## Load in required packages ##
 library(tidyverse)
 library(ggplot2)
@@ -41,24 +43,6 @@ min_verticaltransmission = 0
 max_verticaltransmission = 0
 mu_verticaltransmission = 0
 vertical_transmission_rate = 0.5
-  
-output <- tibble(
-  total_payoff = NA,
-  learning_speed = NA,
-  allocation = NA,
-  generalisation = NA,
-  brain_size = NA,
-  vertical_transmission = NA,
-  Lifespan = NA, 
-  Baseline = NA, 
-  prob_A = NA,
-  prob_B = NA,
-  max_A = NA,
-  max_B = NA,
-  run = run,
-  generation = NA,
-  P = NA,
-)
 
 Difference = max_baseline - min_baseline # What is the difference between the minimum and maximum baselines?
 
@@ -75,84 +59,66 @@ vertical_transmissions <- seq(from = min_verticaltransmission, to = max_vertical
 lifespans <- seq(from = min_lifespan, to = max_lifespan+1, by = 1) # This workaround is to solve a bug in the sampling
 
 # 1) Sample values for a fresh generation
-current_population <- tibble(total_payoff = 0,
-                             learning_speed = sample(learning_speeds, size = N, replace = T),
-                             allocation = sample(allocations, size = N, replace = T),
-                             generalisation = sample(generalisations, size = N, replace = T),
-                             brain_size = sample(brain_sizes, size = N, replace = T),
-                             vertical_transmission = sample(vertical_transmissions, size = N, replace = T)) %>%
+current_population <- data.frame(
+  total_payoff = rep(0, N),
+  learning_speed = sample(learning_speeds, size = N, replace = T),
+  allocation = sample(allocations, size = N, replace = T),
+  generalisation = sample(generalisations, size = N, replace = T),
+  brain_size = sample(brain_sizes, size = N, replace = T),
+  vertical_transmission = sample(vertical_transmissions, size = N, replace = T)) %>%
   mutate(Lifespan = sample(lifespans, size = N, replace = T),
-         Lifespan = ifelse(Lifespan > max_lifespan, Lifespan - 1, Lifespan), # This is part of solving the bug
-         Baseline = 1 - (4 * learning_speed),
-         prob_A = Baseline * allocation,
-         prob_B = Baseline * (1 - allocation),
-         max_A = ((brain_size * 10) * allocation) + 1,
-         max_B = ((brain_size * 10) * (1 - allocation) + 1),
-         max_A = 2000, ## NOTE. If we want to allow allocation strategy to also increase learning capacity, delete these lines.
-         max_B = 2000)
+  Lifespan = ifelse(Lifespan > max_lifespan, Lifespan - 1, Lifespan), # This is part of solving the bug
+  Baseline = 1 - (4 * learning_speed),
+  prob_A = Baseline * allocation,
+  prob_B = Baseline * (1 - allocation),
+  max_A = ((brain_size * 10) * allocation) + 1,
+  max_B = ((brain_size * 10) * (1 - allocation) + 1),
+  max_A = rep(2000, N), ## NOTE. If we want to allow allocation strategy to also increase learning capacity, delete these lines.
+  max_B = rep(2000,N)
+)
 
 # 2) Start the model for the current generation
 
 for(gen in 1:generations){
   
-  # A_prob <- current_population$prob_A
-  # B_prob <- current_population$prob_B
-  # A_max <- current_population$max_A
-  # B_max <- current_population$max_B
+  A_prob <- current_population$prob_A
+  B_prob <- current_population$prob_B
+  A_max <- current_population$max_A
+  B_max <- current_population$max_B
+  generalisation <- current_population$generalisation
+  learning_speed <- current_population$learning_speed
+  allocation <- current_population$allocation
+  L <- current_population$Lifespan
   
-  for(agent in 1:N) { # For each agent in the population.
-    
-    # Pull out their information
-    A_prob <- current_population$prob_A[agent]
-    B_prob <- current_population$prob_B[agent]
-    A_max <- current_population$max_A[agent]
-    B_max <- current_population$max_B[agent]
-    generalisation <- current_population$generalisation[agent]
-    learning_speed <- current_population$learning_speed[agent]
-    allocation <- current_population$allocation[agent]
-    L <- current_population$Lifespan[agent]
-    
-    # Reset payoffs
-    total_payoff <- 0
-    Tasks <- sample(c("A", "B"), size = L, replace = T, prob = c(P, 1-P))
-    
-    for(task in Tasks){
-      Success <- ifelse(task == "A", rbinom(1, size = 1, prob = log_to_prob(A_prob)), rbinom(1, size = 1, prob = log_to_prob(B_prob)))
-      total_payoff <- total_payoff + Success
-      
-      # Learning about the tasks
-      if(task == "A"){
-        A_prob <- A_prob + (learning_rate * learning_speed * allocation)
-        B_prob <- B_prob + (learning_rate * learning_speed * allocation * generalisation)
-      } else if(task == "B"){
-        B_prob <- B_prob + (learning_rate * learning_speed * (1 - allocation))
-        A_prob <- A_prob + (learning_rate * learning_speed * (1 - allocation) * generalisation)
+  current_population$total_payoff <- rep(0, N)
+  
+  if (hack) {
+    Tasks <- rbinom(N, L, P)
+    current_population$total_payoff <- rbinom(N, Tasks, log_to_prob(A_prob)) + rbinom(N, L-Tasks, log_to_prob(B_prob))
+    A_prob <- min(A_prob + Tasks*(learning_rate * learning_speed * allocation) + (L-Tasks)*(learning_rate * learning_speed * (1-allocation) * generalisation), A_max)
+    B_prob <- min(B_prob + (L-Tasks)*(learning_rate * learning_speed * allocation * generalisation) + Tasks*(learning_rate * learning_speed * (1-allocation)), B_max)
+  } else {
+    for(agent in 1:N) { # For each agent in the population.
+      Tasks <- rbinom(L[agent], 1, P)
+      for(task in Tasks){
+        current_population$total_payoff[agent] <- rbinom(1, 1, prob = (task*log_to_prob(A_prob[agent])) + (1-task)*log_to_prob(B_prob[agent]))
+        
+        A_prob[agent] <- min(A_prob[agent] + task*(learning_rate * learning_speed[agent] * allocation[agent]) + (1-task)*(learning_rate * learning_speed[agent] * (1-allocation[agent]) * generalisation[agent]), A_max)
+        B_prob[agent] <- min(B_prob[agent] + (1-task)*(learning_rate * learning_speed[agent] * allocation[agent] * generalisation[agent]) + task*(learning_rate * learning_speed[agent] * (1-allocation[agent])), B_max)
       }
-      
-      # Constrain probabilities based on brain size allocation
-      A_prob <- min(A_prob, A_max)
-      B_prob <- min(B_prob, B_max)  
-    } # End of agent's lifespan
-    
-    # Record the agent's payoff and update their A and B probs 
-    current_population$total_payoff[agent] <- current_population$total_payoff[agent] + total_payoff
-    current_population$prob_A[agent] <- A_prob
-    current_population$prob_B[agent] <- B_prob
-    
-  } # End of agent loop
+    }
+  }
   
   # 4) Apply the fitness penalties
   # NOTE - Need to include brain size if we allow this to evolve also
   
-  current_population$total_payoff <- current_population$total_payoff - (current_population$learning_speed * cost + current_population$generalisation * cost + current_population$vertical_transmission * cost) * current_population$Lifespan
+  current_population$total_payoff <- pmax(current_population$total_payoff - cost*(current_population$learning_speed + current_population$generalisation + current_population$vertical_transmission) * current_population$Lifespan, 0.001)
   
   # Add extra fitness in case of negative fitness values
-  current_population$total_payoff <- (current_population$total_payoff + 10) * selection_strength
   
-  end_population <- current_population %>%
-    mutate(run = run, generation = gen, P = P)
-  
-  output <- rbind(output, end_population)
+  # selection strength won't do anything here
+  # current_population$total_payoff <- (current_population$total_payoff + 10) * selection_strength 
+
   
   # 5) Sample for the next generation
   
@@ -175,36 +141,13 @@ for(gen in 1:generations){
   B_bonus <- new_population$prob_B * (new_population$vertical_transmission * vertical_transmission_rate)
   
   # 6) Mutation. (maybe could be optimised somehow, not sure how)
-  mutation_learning <- runif(N, 0, 1) < mu_learningspeed
-  mutation_allocation <- rep(sample(c(TRUE, FALSE), N, prob = c(mu_allocation, 1-mu_allocation), replace = T)) 
-  mutation_generalisation <- rep(sample(c(TRUE, FALSE), N, prob = c(mu_generalisation, 1-mu_generalisation), replace = T))
-  mutation_brain <- rep(sample(c(TRUE, FALSE), N, prob = c(mu_brainsize, 1-mu_brainsize), replace = T))
-  mutation_vertical <- rep(sample(c(TRUE, FALSE), N, prob = c(mu_verticaltransmission, 1-mu_verticaltransmission), replace = T))      
-  mutation_lifespan <- rep(sample(c(TRUE, FALSE), N, prob = c(mu_lifespan, 1-mu_lifespan), replace = T))
-  
-  ## Mutate learning speed ## 
-  new_population$learning_speed[mutation_learning] <- round(new_population$learning_speed[mutation_learning] + rnorm(sum(mutation_learning), mean = 0, sd = 0.1),2)
-  new_population$learning_speed <- pmin(pmax(new_population$learning_speed, min_learningspeed), max_learningspeed) # Limit learning_speed within the bounds        
-  
-  ## Mutate allocation ##
-  new_population$allocation[mutation_allocation] <- round(new_population$allocation[mutation_allocation] + rnorm(sum(mutation_allocation), mean = 0, sd = 0.1),2)
-  new_population$allocation <- pmin(pmax(new_population$allocation, min_allocation), max_allocation)
-  
-  ## Mutate generalisation ##
-  new_population$generalisation[mutation_generalisation] <- round(new_population$generalisation[mutation_generalisation] + rnorm(sum(mutation_generalisation), mean = 0, sd = 0.1),2)
-  new_population$generalisation <- pmin(pmax(new_population$generalisation, min_generalisation), max_generalisation)
-  
-  ## Mutate brain size ##
-  new_population$brain_size[mutation_brain] <- round(new_population$brain_size[mutation_brain] + rnorm(sum(mutation_brain), mean = 0, sd = 0.1),2)
-  new_population$brain_size <- pmin(pmax(new_population$brain_size, min_brainsize), max_brainsize) 
-  
-  ## Mutate vertical transmission ## 
-  new_population$vertical_transmission[mutation_vertical] <- round(new_population$vertical_transmission[mutation_vertical] + rnorm(sum(mutation_vertical), mean = 0, sd = 0.1),2)
-  new_population$vertical_transmission <- pmin(pmax(new_population$vertical_transmission, min_verticaltransmission), max_verticaltransmission) 
-  
-  ## Mutate Lifespan ## 
-  new_population$Lifespan[mutation_lifespan] <- round(new_population$Lifespan[mutation_lifespan] + rnorm(sum(mutation_lifespan), mean = 0, sd = 3))
-  new_population$Lifespan <- pmin(pmax(new_population$Lifespan, min_lifespan), max_lifespan) 
+
+  new_population$learning_speed <- pmin(pmax(new_population$learning_speed + rnorm(N, mean = 0, sd = 0.1), min_learningspeed), max_learningspeed)
+  new_population$allocation <- pmin(pmax(new_population$allocation + rnorm(N, mean = 0, sd = 0.1), min_allocation), max_allocation)
+  new_population$generalisation <- pmin(pmax(new_population$generalisation + rnorm(N, mean = 0, sd = 0.1), min_generalisation), max_generalisation)
+  new_population$brain_size <- pmin(pmax(new_population$brain_size + rnorm(N, mean = 0, sd = 0.1), min_brainsize), max_brainsize)
+  new_population$vertical_transmission <- pmin(pmax(new_population$vertical_transmission + rnorm(N, mean = 0, sd = 0.1), min_verticaltransmission), max_verticaltransmission)
+  new_population$Lifespan <- pmin(pmax(new_population$Lifespan + rnorm(N, mean = 0, sd = 0.1), min_lifespan), max_lifespan)
   
   ## Recalculate the A max and B max based on brain size and allocation strategy
   
